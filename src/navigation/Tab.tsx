b0e16a2bf5b8af6933b7c419484508a05c467efa
ucankalecansu <cansu@bitsody.com>
2024-01-25 19:06:20 +0300

    tabs

--- src/navigation/Tab.tsx
+++ src/navigation/Tab.tsx
@@ -1,105 +1,85 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import React from 'react'
-
+import React from 'react';
 import Ionicons from '@expo/vector-icons/Ionicons';
-import { Image, TouchableOpacity,View,StyleSheet } from 'react-native';
+import { Image } from 'react-native';
 import DiscoverScreen from '../screens/DiscoverScreen';
 import CustomScreen from '../screens/CustomScreen';
 import WalletScreen from '../screens/WalletScreen';
+import styles from './styles';
 
 const Tabs = () => {
+  const Tab = createBottomTabNavigator();
 
-    const Tab = createBottomTabNavigator();
-
-    const CustomTabBarButton=({children,onPress})=>(
-      <TouchableOpacity style={{
-        top:-30,
-        justifyContent:'center',
-        alignItems:'center',
-        ...styles.shadow
-
-      }} onPress={onPress}>
-        <View>
-          {children}
-        </View>
-      </TouchableOpacity>
-    )
+  const screenOptions = {
+    tabBarStyle: {
+      backgroundColor:"#fff",
+      borderRadius:15,
+      height:70,
+    },
+  };
 
   return (
-    <Tab.Navigator >
-      <Tab.Screen name="Discover" component={DiscoverScreen} 
-        options={()=>({
-          tabBarIcon:({color,size})=>{
-              return <Ionicons color={color} size={50} name={"compass"} />
-
-          },
-          tabBarInactiveTintColor:"gray",
+    <Tab.Navigator screenOptions={screenOptions} >
+      <Tab.Screen
+        name="Discover"
+        component={DiscoverScreen}
+        options={() => ({
+          tabBarIcon: ({ color }) => (
+            <Ionicons color={color} size={50} name={'compass'} />
+          ),
+          tabBarInactiveTintColor: 'gray',
           headerShown: false,
-          tabBarStyle:{
-            height:70,
-            borderTopLeftRadius:20,
-            borderTopRightRadius:20,
-            ...styles.shadow
-          }
-      })}
-      
+         
+        })}
       />
-      <Tab.Screen name="Custom" component={CustomScreen}
+      <Tab.Screen
+        name="Custom"
+        component={CustomScreen}
+        options={() => ({
       
-      options={()=>({
-        tabBarIcon:({color,size})=>{
-            return   <Image
-            source={require("../../assets/plus.png")}
-            resizeMode="contain"
-            style={{
-              width: 50,
-              height: 50,
-              backgroundColor: "red",
-              shadowColor: '#000',
-              shadowOffset: { width: 0, height: 2 },
-              shadowOpacity: 0.8,
-              shadowRadius: 4,
-              padding: 10, 
-            }}
-          />
-
-        },
-        tabBarInactiveTintColor:"gray",
-        headerShown: false,
-        tabBarButton:(props)=>(
-        <CustomTabBarButton onPress={props.onPress} children={props.children}/>
-        )
-    })}/>
-      <Tab.Screen name="Wallet" component={WalletScreen}  options={()=>({
-          tabBarIcon:({color,size})=>{
-              return <Ionicons color={color} size={50} name={"star"} />
-
-          },
-          tabBarInactiveTintColor:"gray",
+          tabBarIcon: () => (
+            <Image
+              source={require('../../assets/plus.png')}
+              resizeMode="stretch"
+              style={styles.image}
+            />
+          ),
+          tabBarInactiveTintColor: 'gray',
           headerShown: false,
-          tabBarStyle:{
+          tabBarIconStyle:{
+            position:"absolute",
+            top:-30,
+            borderBottomColor:"orange",
+            borderWidth:3,
+            width:70,
             height:70,
-            borderTopLeftRadius:20,
-            borderTopRightRadius:20
-          }
-      })}/>
+            borderTopColor:"green",
+            borderRadius:10,
+            borderRightColor:"yellow",
+            borderLeftColor:"red",            
+          },
+          title: '',
+        })}
+      />
+      <Tab.Screen
+        name="Wallet"
+        component={WalletScreen}
+        options={() => ({
+          tabBarIcon: ({ color }) => (
+            <Ionicons color={color} size={50} name={'star'} />
+          ),
+          tabBarInactiveTintColor: 'gray',
+        
+        })}
+      />
     </Tab.Navigator>
-  )
-}
+  );
+};
 
-export default Tabs
+export default Tabs;
 
 
 
-const styles=StyleSheet.create({
-  shadow:{
-    shadowColor:"#7f5df0",
-    shadowOffset:{
-      width:0,
-      height:10
-    },
-    shadowOpacity:0.25,
-    shadowRadius:3.5,
-    elevation:5
-  }
-})
+
+
+
---
+++ src/navigation/styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    image:{
+        width:60,
+        height:60
+    }
+
+  });
+
+export default styles;
